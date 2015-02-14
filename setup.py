dc9bff6b73b104c3fb67859dc41283b81832117f
Chenlong <hustcl1024@gmail.com>
2015-02-14 13:47:35 +0800

    添加有道翻译

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.1.3'
+version = '0.1.4'
 
 setup(name='youdao',
       version=version,
--- youdao/youdao_web.py
+++ youdao/youdao_web.py
@@ -2,22 +2,21 @@
 
 import requests
 import re
+import json
 from bs4 import BeautifulSoup
 
 
 class youdao_web:
     web_url = 'http://dict.youdao.com/search?keyfrom=dict.top&q='
+    translation_url = 'http://fanyi.youdao.com/translate?keyfrom=dict.top&i='
     result = {
-        "basic": {
-            "explains": []
-        },
         "query": "",
         "errorCode": 0,
-        "web": []
     }
 
 
     def get_result(self, word):
+        self.word = word
         r = requests.get(self.web_url + word)
         r.raise_for_status()
         self.parse(r.text)
@@ -27,28 +26,41 @@ class youdao_web:
         soup = BeautifulSoup(html)
         root = soup.find(id='results-contents')
 
-        self.result['query'] = unicode(root.find(class_='keyword').string)
-        basic = root.find(id='phrsListTab')
-        if not basic:
-            del self.result['basic']
+        #query 搜索的关键字
+        keyword = root.find(class_='keyword')
+        if not keyword:
+            self.result['query'] = self.word
         else:
+            self.result['query'] = unicode(keyword.string)
+
+        #基本解释
+        basic = root.find(id='phrsListTab')
+        if basic:
             trans = basic.find(class_='trans-container')
-            self.result['basic']['explains'] = [unicode(tran.string) for tran in trans.find_all('li')]
+            if trans:
+                self.result['basic'] = {}
+                self.result['basic']['explains'] = [unicode(tran.string) for tran in trans.find_all('li')]
+                #中文
+                if len(self.result['basic']['explains']) == 0:
+                    exp = trans.find(class_='wordGroup').stripped_strings
+                    self.result['basic']['explains'].append(' '.join(exp))
 
-            #中文
-            if len(self.result['basic']['explains']) == 0:
-                exp = trans.find(class_='wordGroup').stripped_strings
-                self.result['basic']['explains'].append(' '.join(exp))
+                #音标
+                phons = basic(class_='phonetic', limit=2)
+                if len(phons) == 2:
+                    self.result['basic']['uk-phonetic'], self.result['basic']['us-phonetic'] = \
+                        [unicode(p.string)[1:-1] for p in phons]
+                elif len(phons) == 1:
+                    self.result['basic']['phonetic'] = unicode(phons[0].string)[1:-1]
 
-            #音标
-            phons = basic(class_='phonetic', limit=2)
-            if len(phons) == 2:
-                self.result['basic']['uk-phonetic'], self.result['basic']['us-phonetic'] = \
-                    [unicode(p.string)[1:-1] for p in phons]
-            elif len(phons) == 1:
-                self.result['basic']['phonetic'] = unicode(phons[0].string)[1:-1]
+        #翻译
+        if 'basic' not in self.result:
+            self.result['translation'] = self.get_translation(self.word)
 
-            web = root.find(id='webPhrase')
+
+        #网络释义(短语)
+        web = root.find(id='webPhrase')
+        if web:
             self.result['web'] = [
                 {
                     'key': unicode(wordgroup.find(class_='search-js').string).strip(),
@@ -60,8 +72,22 @@ class youdao_web:
             #             'value': [v.strip() for v in unicode(wordgroup.find('span').next_sibling).split(';')]}
             #     self.result['web'].append(item)
 
+    def get_translation(self, word):
+        """
+        通过web版有道翻译抓取翻译结果
+        :param word:str 关键字
+        :return:list 翻译结果
+        """
+        r = requests.get(self.translation_url+word)
+        if r.status_code != requests.codes.ok:
+            return None
+
+        pattern = re.compile(r'"translateResult":\[(\[.+\])\]')
+        m = pattern.search(r.text)
+        result = json.loads(m.group(1))
+        return [item['tgt'] for item in result]
 
 
 if __name__ == '__main__':
     test = youdao_web()
-    print test.get_result('test')
+    print test.get_result('what the fuck')
